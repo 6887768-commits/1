0599e36683d5c7b5852b76b2e9699e3c1e463786
6887768-commits <6887768@gmail.com>
2026-01-22 19:33:28 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+
+const CACHE_NAME = 'userhub-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/index.tsx',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+// 安装时缓存核心资源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 激活并清理旧缓存
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 策略：缓存优先，后台更新 (Stale-while-revalidate)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
